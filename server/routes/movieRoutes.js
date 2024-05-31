6ecc0703d8e41adc4004682c8645c5cd463eec41
Rajesh Korishettar <rajeshlk16@gmail.com>
2024-05-31 22:23:39 +0530

    movies CRUD Operations updated

--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -35,4 +35,37 @@ router.get('/getAllMovies', async(req, res) => {
     }
 })
 
+router.post('/delete', async(req, res) => {
+    try {
+        await Movie.findByIdAndDelete(req.body.movieId)
+        res.send({
+            success: true,
+            message: 'Movie deleted succesfully'
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: 'Something went wrong',
+            data: error
+        })
+        
+    }
+})
+
+router.put('/update', async(req, res) => {
+    try {
+        const movie = await Movie.findByIdAndUpdate(req.body.movieId, req.body)
+        res.send({
+            success: true,
+            message: 'Movie updated succesfully'
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: 'Something went wrong',
+            data: error
+        })
+    }
+})
+
 exports.router = router
